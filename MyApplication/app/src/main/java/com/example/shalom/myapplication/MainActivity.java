07e30b2135574e7e1f460b913e645f36beaca631
ee bb <ezra@anyblock.com>
2016-11-17 16:39:57 +0200

    www

--- MyApplication/app/src/main/java/com/example/shalom/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/shalom/myapplication/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends Activity {
     protected void onRestart() {
         super.onRestart();
         android.util.Log.d(ACTIVITY_LIFE_TAG , "onRestart()");
+        int q;
     }
 
     @Override
