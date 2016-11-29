745548ce27f1e87a32046b84cca9a47ff872191f
yedidya <yedidyakfir@gmail.com>
2016-11-29 20:53:12 +0200

    Yadidya connect

--- MyApplication/app/src/main/java/com/example/shalom/myapplication/datasource/ListDataSource.java
+++ MyApplication/app/src/main/java/com/example/shalom/myapplication/datasource/ListDataSource.java
@@ -1,8 +1,11 @@
 package com.example.shalom.myapplication.datasource;
 
+import android.app.Activity;
+
 import com.example.shalom.myapplication.entities.Business;
 import com.example.shalom.myapplication.entities.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,7 +14,34 @@ import java.util.List;
 
 public class ListDataSource
 {
-    public List<Business> businesses;
-    public List<com.example.shalom.myapplication.Activity> activities;
-    public List<User> users;
+    public ArrayList<Business> businesses;
+    public ArrayList<com.example.shalom.myapplication.Activity> activities;
+    public ArrayList<User> users;
+
+    public void addUser(User user)
+    {
+        users.add(user);
+    }
+
+    public void addBusiness(Business b)
+    {
+        businesses.add(b);
+    }
+
+    public void addActivity(com.example.shalom.myapplication.Activity activity)
+    {
+        activities.add(activity);
+    }
+
+    public ArrayList<Business> getBusinesses() {
+        return businesses.clone();
+    }
+
+    public ArrayList<com.example.shalom.myapplication.Activity> getActivities() {
+        return activities.clone();
+    }
+
+    public ArrayList<User> getUsers() {
+        return users.clone();
+    }
 }
