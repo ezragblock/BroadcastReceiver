37e2476a16834cf03161a6404c9d5c4603a68acd
ee bb <ezra@anyblock.com>
2016-11-30 13:20:07 +0200

    check  what cursor is and if it's relevant to the datasource even though there isn't a sql database yet

--- MyApplication/app/src/main/java/com/example/shalom/myapplication/datasource/IDataSource.java
+++ MyApplication/app/src/main/java/com/example/shalom/myapplication/datasource/IDataSource.java
@@ -1,5 +1,7 @@
 package com.example.shalom.myapplication.datasource;
 
+import android.database.Cursor;
+
 import com.example.shalom.myapplication.entities.Activity;
 import com.example.shalom.myapplication.entities.Business;
 import com.example.shalom.myapplication.entities.User;
@@ -13,4 +15,7 @@ public interface IDataSource
     void addUser(User user);
     void addActivity(Activity activity);
     void addBusiness(Business user);
+    //public abstract Cursor getUsers();
+    //public abstract Cursor getActivities();
+    //public abstract Cursor getBusinesses();
 }
