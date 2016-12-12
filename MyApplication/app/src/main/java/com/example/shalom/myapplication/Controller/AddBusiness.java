9696282f09298df163b9de4933ea05d594b071e3
ee bb <ezra@anyblock.com>
2016-12-12 23:38:00 +0200

    Created window for creating businesses. However, it doesn't add it acrually to the database cause it doesn't use the content provider.

---
+++ MyApplication/app/src/main/java/com/example/shalom/myapplication/Controller/AddBusiness.java
@@ -0,0 +1,44 @@
+package com.example.shalom.myapplication.Controller;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.shalom.myapplication.R;
+import com.example.shalom.myapplication.model.entities.Address;
+import com.example.shalom.myapplication.model.entities.Business;
+
+public class AddBusiness extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_business);
+        Button createBusinessBtn = (Button)findViewById(R.id.createBusiness);
+        createBusinessBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Address address = new Address(
+                        ((EditText) findViewById(R.id.state)).getText().toString()
+                        ,((EditText) findViewById(R.id.city)).getText().toString()
+                        ,((EditText) findViewById(R.id.street)).getText().toString());
+
+                Business newBusiness = new Business(
+                        Integer.parseInt(((EditText) findViewById(R.id.ID)).getText().toString())
+                        ,((EditText) findViewById(R.id.name)).getText().toString()
+                        ,address
+                        ,((EditText) findViewById(R.id.telephoneNumber)).getText().toString()
+                        ,((EditText) findViewById(R.id.email)).getText().toString()
+                        ,((EditText) findViewById(R.id.website)).getText().toString());
+
+            }
+        });
+    }
+
+}
