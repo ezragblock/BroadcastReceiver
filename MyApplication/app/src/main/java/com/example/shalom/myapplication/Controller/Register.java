824d3867935f413d44d9cdc8fe63a4bc24d242c4
yedidya <yedidyakfir@gmail.com>
2017-01-12 16:47:56 +0200

    ,

--- MyApplication/app/src/main/java/com/example/shalom/myapplication/Controller/Register.java
+++ MyApplication/app/src/main/java/com/example/shalom/myapplication/Controller/Register.java
@@ -2,6 +2,7 @@ package com.example.shalom.myapplication.Controller;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,8 @@ import android.widget.EditText;
 
 import com.example.shalom.myapplication.R;
 import com.example.shalom.myapplication.SharedPreference.MyPreference;
+import com.example.shalom.myapplication.model.backend.ActivateBackEndTask;
+import com.example.shalom.myapplication.model.backend.ICallableTask;
 import com.example.shalom.myapplication.model.datasource.CustomContentProvider;
 import com.example.shalom.myapplication.model.entities.User;
 
@@ -27,7 +30,7 @@ public class Register extends AppCompatActivity
 
     public void register(View v)
     {
-        Uri uri = Uri.parse("content://" + CustomContentProvider.PROVIDER_NAME + "/users");
+        final Uri uri = Uri.parse("content://" + CustomContentProvider.PROVIDER_NAME + "/users");
         ArrayList<User> users = User.getListFromCursor(getContentResolver().query(uri,null,null,null,null));
         EditText username = ((EditText)findViewById(R.id.username));
         for (User user: users)
@@ -38,8 +41,16 @@ public class Register extends AppCompatActivity
                 return;
             }
         }
-        User u = new User(username.getText().toString(),((EditText)findViewById(R.id.Password)).getText().toString());
-        getContentResolver().insert(uri,u.getContentValue());
+        final User u = new User(username.getText().toString(),((EditText)findViewById(R.id.Password)).getText().toString());
+
+        new ActivateBackEndTask().execute(new ICallableTask()
+        {
+            public Cursor Activate()
+            {
+                getContentResolver().insert(uri,u.getContentValue());
+                return null;
+            }
+        });
 
         if(((CheckBox)findViewById(R.id.SaveOnPhone)).isActivated())
         {
