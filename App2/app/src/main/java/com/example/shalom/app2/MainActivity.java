864c15aaf06857d1002bf272010210246ec5fd57
ee bb <ezragblock@gmail.com>
2017-01-18 00:06:56 +0200

    statrted broadcast receiver again

--- App2/app/src/main/java/com/example/shalom/app2/MainActivity.java
+++ App2/app/src/main/java/com/example/shalom/app2/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.shalom.app2;
-
+import com.example.shalom.app2.R;
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void broadcastCustomIntent(View view)
+    {
+        Intent intent = new Intent("MyCustomIntent");
+
+        EditText et = (EditText)findViewById(R.id.extraIntent);
+        // add data to the Intent
+        intent.putExtra("message", (CharSequence)et.getText().toString());
+        intent.setAction("com.example.shalom.app2.A_CUSTOM_INTENT");
+        sendBroadcast(intent);
     }
 }
