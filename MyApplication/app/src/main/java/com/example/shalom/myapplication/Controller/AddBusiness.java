1017f2f8093ec786a577f6505b71945eda124b6d
yedidya <yedidyakfir@gmail.com>
2017-01-18 21:01:35 +0200

    שינויים קלים

--- MyApplication/app/src/main/java/com/example/shalom/myapplication/Controller/AddBusiness.java
+++ MyApplication/app/src/main/java/com/example/shalom/myapplication/Controller/AddBusiness.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.shalom.myapplication.R;
 import com.example.shalom.myapplication.model.backend.ActivateBackEndTask;
